7fbcadf14d9f13628376a68f0d87f7acb82afc99
Ed <edwardaacowell@gmail.com>
2021-01-22 15:26:59 +0000

    added files to host on heroku

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,27 @@
-const ENV = process.env.NODE_ENV || 'development';
+const { DB_URL } = process.env;
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations',
+    directory: "./db/migrations",
   },
   seeds: {
-    directory: './db/seeds',
+    directory: "./db/seeds",
   },
 };
 
 const customConfigs = {
-  development: { connection: { database: 'away_days' } },
-  test: { connection: { database: 'away_days_test' } },
+  development: { connection: { database: "away_days" } },
+  test: { connection: { database: "away_days_test" } },
+  production: {
+    connection: {
+      connectionString: DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
 
 module.exports = { ...baseConfig, ...customConfigs[ENV] };
