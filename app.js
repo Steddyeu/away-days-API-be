86741178b1ebc91a53d635f930d278725d6ee258
Ed <edwardaacowell@gmail.com>
2021-01-25 15:56:14 +0000

    installed cors for fe

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
-const { handle404s, handlePSQLErrors, handleCustomError } = require("./controllers/errorHandling");
+const cors = require("cors");
+const {
+  handle404s,
+  handlePSQLErrors,
+  handleCustomError,
+} = require("./controllers/errorHandling");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -9,5 +16,5 @@ app.use("/api", apiRouter);
 
 app.use("*", handle404s);
 app.use(handlePSQLErrors);
-app.use(handleCustomError)
+app.use(handleCustomError);
 module.exports = app;
